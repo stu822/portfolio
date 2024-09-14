b6c4c3a9f1c9cb2475b4ec31d52edf07f8543890
Stuart <stu822@gmail.com>
2024-09-14 17:02:21 -0500

    create mobile navbar and hamburger icon

--- app.js
+++ app.js
@@ -3,6 +3,11 @@ const modalWindow = document.querySelector(".modal-window");
 const modalBtn = document.querySelector(".modal button");
 const modals = document.querySelectorAll(".modal");
 const cards = document.querySelectorAll(".card");
+const hamburger = document.querySelector(".hamburger-menu")
+const mobileNavBar = document.querySelector(".mobile-nav")
+const barOne = document.querySelector(".bar-1")
+const barTwo = document.querySelector(".bar-2")
+const barThree = document.querySelector(".bar-3")
 
 for(let i = 0; i < cards.length; i++){
    cards[i].addEventListener("click", function(){
@@ -25,3 +30,10 @@ for(let modal of modals){
         e.stopPropagation()
     })
 }
+
+hamburger.addEventListener("click", ()=>{
+    mobileNavBar.classList.toggle("display-nav")
+    barOne.classList.toggle("bar-1-transform")
+    barTwo.classList.toggle("bar-2-transform")
+    barThree.classList.toggle("bar-3-transform")
+})
